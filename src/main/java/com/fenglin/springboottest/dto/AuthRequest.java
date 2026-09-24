7ea009c7284138619a69fdf5d1a9c8fe06ee3d60
xyy123-afk <1709678401@qq.com>
2026-09-24 14:55:14 +0800

    Delete src directory

--- src/main/java/com/fenglin/springboottest/dto/AuthRequest.java
+++
@@ -1,7 +0,0 @@
-package com.fenglin.springboottest.dto;
-
-/**
- * 注册 / 登录请求体：{ username, password }
- */
-public record AuthRequest(String username, String password) {
-}
--- src/main/java/com/fenglin/springboottest/dto/TodoAddRequest.java
+++
@@ -1,7 +0,0 @@
-package com.fenglin.springboottest.dto;
-
-/**
- * 新增待办请求体：{ username, content }
- */
-public record TodoAddRequest(String username, String content) {
-}
